e9664ee60114e77b662be606495b44ccda24ced4
Dusty <dustymccord@gmail.com>
2020-01-28 20:58:50 -0800

    set up constructor funtion for Bank and accounts. It will add constructor functions for deposits and withdrawals.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
+function MasterBank() {
+	this.account = [];
+	this.accountID = 0;
+}
+
+function BankAccount(userName, balance) {
+	this.userName = userName;
+	this.currentBalance = balance;
+	this.accountId;
+}
+
+BankAccount.prototype.makeDeposit = function(amount) {
+	this.currentBalance += amount;
+};
+
+BankAccount.prototype.makeWithdrawal = function(amount) {
+	this.currentBalance -= amount;
+};
+
+BankAccount.prototype.getBalance = function() {
+	return this.currentBalance;
+};
+
 $(document).ready(function() {
 	console.log('JavaScipt is working');
 
